74a290a4ccc7bb9282befeb2cf712e9de1bc55a6
pourya <pourya_1999@yahoo.com>
2021-06-10 02:56:37 +0430

    matlab

--- amar.R
+++ amar.R
@@ -1,5 +1,6 @@
 library("Rlab")
 height = rnorm(200, mean = 170, sd = 9.4)
 sex = rbern(200, prob = 0.7)
-error = 
-print(error)
+error = runif(200, min = 85, max = 100)
+print(height - error)
+#print(weight)
