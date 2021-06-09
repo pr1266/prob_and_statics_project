fb4f63f1e8ee7a2b625ea694d0f16c5ffee32a00
pourya <pourya_1999@yahoo.com>
2021-06-10 03:44:58 +0430

    freq

--- amar.R
+++ amar.R
@@ -1,10 +1,12 @@
 library("Rlab")
 library("DescTools")
 library("EnvStats")
+library("summarytools")
+
 height = rnorm(200, mean = 170, sd = 9.4)
 sex = rbern(200, prob = 0.7)
 error = runif(200, min = 85, max = 100)
-weight = lapply(height, '-', error)
+weight = sapply(height, '-', error)
 
-k = 1 + log(200,2)
-print(k)
+k = floor(1 + log(200, 2))
+weight_freq = freq(weight, breaks = k)
